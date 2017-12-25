aac9cc79f9cfd61ca281f59b29e2a1b5ed4f36ee
Dmitry Frank <mail@dmitryfrank.com>
2017-12-25 18:02:30 +0200

    Add crontab docs
    PUBLISHED_FROM=b4139a8ade67dc9bc88525064b3b864919d09a4c

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Crontab
+
+Crontab wraps [cron core](https://github.com/mongoose-os-libs/cron) and
+maintains a persisted set of cron jobs. Crontab file is simply a JSON file
+(actually managed by [jstore](https://github.com/mongoose-os-libs/jstore))
+which looks like this:
+
+```json
+{"items":[
+  ["1", {
+    "at": "0 0 7 * * MON-FRI",
+    "enable": true,
+    "action": "foo",
+    "payload": {"a": 1, "b": 2}
+  }],
+  ["2", {
+    "at": "0 */2 1-4 * * *",
+    "enable": true,
+    "action": "bar"
+  }]
+]}
+```
+
+This file is maintained by a set of API functions, see [API
+documentation](https://mongoose-os.com/docs/api/mgos_crontab.h.html) for more
+details.
+
+For the cron expression syntax, see [cron
+core](https://github.com/mongoose-os-libs/cron) docs.
--- include/mgos_crontab.h
+++ include/mgos_crontab.h
@@ -3,6 +3,60 @@
  * All rights reserved
  */
 
+/*
+ * Crontab wraps [cron core](https://github.com/mongoose-os-libs/cron) and
+ * maintains a persisted set of cron jobs. Crontab file is simply a JSON file
+ * (actually managed by [jstore](https://github.com/mongoose-os-libs/jstore))
+ * which looks like this:
+ *
+ * ```json
+ * {"items":[
+ *   ["1", {
+ *     "at": "0 0 7 * * MON-FRI",
+ *     "enable": true,
+ *     "action": "foo",
+ *     "payload": {"a": 1, "b": 2}
+ *   }],
+ *   ["2", {
+ *     "at": "0 30 23 30 * *",
+ *     "enable": true,
+ *     "action": "bar"
+ *   }]
+ * ]}
+ * ```
+ *
+ * This file is maintained by a set of API functions (see below).
+ *
+ * Obviously, crontab file contains a set of cron jobs, each of which consists,
+ * at least, of the cron expression like `0 0 7 * * MON-FRI` (refer to the
+ * [cron core](https://github.com/mongoose-os-libs/cron) for the expression
+ * syntax docs) and an action to be taken. Action is just a string, in the
+ * example above there are two actions: `foo` and `bar`. Additionally, there
+ * can be a `payload`, which is an arbitrary JSON. Payload is just a set of
+ * parameters for the action.
+ *
+ * Obviously, there should be a mapping between those string actions and the
+ * corresponding functions to be called; this is what
+ * `mgos_crontab_register_handler()` is for.
+ *
+ * Example:
+ *
+ * ```c
+ * static void my_foo_cb(struct mg_str action,
+ *                       struct mg_str payload, void *userdata) {
+ *   LOG(LL_INFO, ("Crontab foo job fired! Payload: %.*s", payload.len,
+ *payload.p));
+ *   (void) action;
+ *   (void) userdata;
+ * }
+ *
+ * // Somewhere else:
+ * mgos_crontab_register_handler("foo", my_foo_cb, NULL);
+ * ```
+ *
+ * The code above maps action `foo` in the JSON to the callback `my_foo_cb`.
+ */
+
 #ifndef CS_MOS_LIBS_CRONTAB_SRC_CRONTAB_H_
 #define CS_MOS_LIBS_CRONTAB_SRC_CRONTAB_H_
 
@@ -103,6 +157,23 @@ bool mgos_crontab_iterate(mgos_crontab_iterate_cb cb, void *userdata,
 
 /*
  * Add a handler for the given string action
+ *
+ * Example:
+ *
+ * ```c
+ * static void my_foo_cb(struct mg_str action,
+ *                       struct mg_str payload, void *userdata) {
+ *   LOG(LL_INFO, ("Crontab foo job fired! Payload: %.*s", payload.len,
+ *payload.p));
+ *   (void) action;
+ *   (void) userdata;
+ * }
+ *
+ * // Somewhere else:
+ * mgos_crontab_register_handler("foo", my_foo_cb, NULL);
+ * ```
+ *
+ * The code above maps action `foo` in the JSON to the callback `my_foo_cb`.
  */
 void mgos_crontab_register_handler(struct mg_str action, mgos_crontab_cb cb,
                                    void *userdata);
